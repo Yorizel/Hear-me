81eaceebb58abd716c737b70b98c3d539c4aa6cf
Yorizel <manoel.netocarvalho03@gmail.com>
2021-03-11 08:20:43 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Routes from "./routes";
-import DashboardLanding from "./pages/dashboard";
-import {AuthContext} from "./context/auth";
+
 
 
 export default function App() {
-    const {auth} = React.useContext(AuthContext);
+
     return (
         <Router>
             <Route path={'*'}>
